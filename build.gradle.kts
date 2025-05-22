a78c9d13d1ad264cbbbbdc67b4647941eec27361
Alex Shepeliev <a.shepeliev@gmail.com>
2025-05-22 12:31:14 +0300

    Add Android target

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
 }
 
 group = "io.voxkit.kotlin.nanoid"
@@ -8,11 +11,49 @@ version = "0.1.0-SNAPSHOT"
 kotlin {
     explicitApi()
 
+    androidTarget {
+        publishAllLibraryVariants()
+        compilerOptions { jvmTarget = JvmTarget.JVM_11 }
+    }
+
     jvm()
 
+    applyDefaultHierarchyTemplate()
+
     sourceSets {
+        val jvmAndAndroidMain by creating {
+            dependsOn(commonMain.get())
+        }
+
+        jvmMain {
+            dependsOn(jvmAndAndroidMain)
+        }
+
+        androidMain {
+            dependsOn(jvmAndAndroidMain)
+        }
+
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
+    }
+}
+
+android {
+    namespace = "io.voxkit.kotlin.nanoid"
+
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    sourceSets["main"].res.srcDir("src/androidMain/res")
+
+    sourceSets["testDebug"].resources.srcDir("src/commonTest/resources")
+    sourceSets["testRelease"].resources.srcDir("src/commonTest/resources")
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
--- src/commonTest/kotlin/io/voxkit/kotlin/nanoid/NanoIdTest.kt
+++ src/commonTest/kotlin/io/voxkit/kotlin/nanoid/NanoIdTest.kt
@@ -9,6 +9,10 @@ class NanoIdTest {
     fun testGenerateNanoId() {
         val id1 = NanoId.generate()
         val id2 = NanoId.generate()
+
+        println("Generated NanoId 1: $id1")
+        println("Generated NanoId 2: $id2")
+
         assertEquals(21, id1.length, "NanoId length should be 21")
         assertEquals(21, id2.length, "NanoId length should be 21")
         assertNotEquals(id1, id2, "Generated NanoIds should be unique")
