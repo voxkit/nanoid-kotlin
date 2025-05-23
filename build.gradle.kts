03ad609e045dcda7d69a20c5b68bb6138edc6337
Alex Shepeliev <shepeliev@users.noreply.github.com>
2025-05-23 17:59:14 +0300

    Prepare for publishing (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,28 @@
 @file:OptIn(ExperimentalWasmDsl::class)
 
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.ktlint)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
-group = "io.voxkit.kotlin.nanoid"
-version = "0.1.0-SNAPSHOT"
+val versionProperties = Properties()
+val versionPropertiesFile = rootDir.resolve("version.properties")
+versionPropertiesFile.takeIf { it.exists() }?.let {
+    versionProperties.load(it.inputStream())
+}
+
+group = "io.voxkit.kotlin"
+version = versionProperties["snapshot.version"]?.let { "$it".removePrefix("v") } ?: "0.0.0"
+
+logger.lifecycle("Selected version name: ${project.version}")
 
 kotlin {
     explicitApi()
@@ -80,18 +92,71 @@ kotlin {
 android {
     namespace = "io.voxkit.kotlin.nanoid"
 
-    compileSdk = libs.versions.compileSdk.get().toInt()
+    compileSdk =
+        libs.versions.compileSdk
+            .get()
+            .toInt()
     sourceSets["main"].res.srcDir("src/androidMain/res")
 
     sourceSets["testDebug"].resources.srcDir("src/commonTest/resources")
     sourceSets["testRelease"].resources.srcDir("src/commonTest/resources")
 
     defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
+        minSdk =
+            libs.versions.minSdk
+                .get()
+                .toInt()
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+
+    coordinates(project.group.toString(), project.name, "${project.version}")
+
+    pom {
+        name = "Nano ID"
+        description = "Nano ID for Kotlin Multiplatform."
+        url = "https://github.com/voxkit/nanoid-kotlin"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        developers {
+            developer {
+                id = "itsmefox"
+                name = "Patrick Bösch"
+                email = "patrick.boesch@viascom.email"
+                organizationUrl = "https://viascom.io/"
+            }
+            developer {
+                id = "nik-sta"
+                name = "Nikola Stankovic"
+                email = "nikola.stankovic@viascom.email"
+                organizationUrl = "https://viascom.io/"
+            }
+            developer {
+                id = "shepeliev"
+                name = "Alex Shepeliev"
+                email = "a.shepeliev@gmail.com"
+            }
+        }
+
+        scm {
+            url = "https://github.com/voxkit/nanoid-kotlin"
+            connection = "scm:git:https://github.com/voxkit/nanoid-kotlin.git"
+            developerConnection = "scm:git:https://github.com/voxkit/nanoid-kotlin.git"
+        }
     }
 }
